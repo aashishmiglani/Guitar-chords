51f034028db48ca62a10bdfb1c9a0ec592c3a5a2
aashishmiglani <aashishmiglani54@gmail.com>
2023-05-24 08:05:05 +0530

    pagination

--- Routes/Routes.js
+++ Routes/Routes.js
@@ -1,10 +1,17 @@
 const express = require("express")
 const server = express()
 const path = require("path")
+const mongoose = require("mongoose")
+
+
 
 
 const router = express.Router()
 server.use(express.urlencoded({ extended: true }))
+
+
+
+
 const creditsSchema = require("../Schemas/creditsSchema")
 const chords_guitarSchema = require("../Schemas/ChordsSchema")
 const xlsx = require("xlsx")
@@ -25,8 +32,33 @@ const uploadExcelPdf = require("../Multer/excelPdfMulter")
 
 //GET APIs (READ)
 router.get("/chords", async (req, res) => {
-    const allCredits = await creditsSchema.find({})
-    res.status(200).json(allCredits)
+    // console.log(req.singer)
+    console.log(req.query.album_name)
+    let pageLimit = Number(req.query.limit)
+    let pageNumber = Number(req.query.page)
+    let skipData = (pageNumber - 1) * pageLimit
+    let allCredits
+
+    req.query.singer ?
+        allCredits = await creditsSchema.find({ singer: new RegExp(req.query.singer, "i") }) :
+        req.query.song_name ?
+            allCredits = await creditsSchema.find({ song_name: new RegExp(req.query.song_name, "i") }) :
+            req.query.album_name ?
+                allCredits = await creditsSchema.find({ album_name: new RegExp(req.query.album_name, "i") }) :
+                allCredits = await creditsSchema.find({}).skip((pageNumber && pageLimit) ? skipData : 0).limit((pageNumber && pageLimit) ? pageLimit : 10)
+
+
+
+    const allCreditsTotalData = await creditsSchema.find({})
+
+    let pageResponse = {
+        "next": pageNumber ? pageNumber + 1 : null,
+        "previous": pageNumber ? pageNumber - 1 : null,
+        "limit": pageLimit ? pageLimit : 10,
+        "totalPages": allCreditsTotalData?.length / pageLimit,
+        "results": allCredits
+    }
+    res.status(200).json(pageResponse)
 
 })
 
@@ -50,19 +82,19 @@ router.post("/chords", async (req, res) => {
 
 
 
-// router.post("/import-chords", uploadExcel.single("excel"), async (req, res) => {
-//     let filename = "./excel/" + req.file.filename
-//     let workbook = xlsx.readFile(filename)
-//     const worksheet = workbook.Sheets[workbook.SheetNames[0]];
-//     const data = xlsx.utils.sheet_to_json(worksheet);
+router.post("/import-chords", uploadExcel.single("excel"), async (req, res) => {
+    let filename = "./excel/" + req.file.filename
+    let workbook = xlsx.readFile(filename)
+    const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+    const data = xlsx.utils.sheet_to_json(worksheet);
 
-//     for (let i = 0; i < data.length; i++) {
-//         const chordsCreditsSchema = new creditsSchema(data[i])
-//         let result = await chordsCreditsSchema.save()
-//     }
-//     res.status(200).json(data)
+    for (let i = 0; i < data.length; i++) {
+        const chordsCreditsSchema = new creditsSchema(data[i])
+        let result = await chordsCreditsSchema.save()
+    }
+    res.status(200).json(data)
 
-// })
+})
 
 
 
--- Schemas/creditsSchema.js
+++ Schemas/creditsSchema.js
@@ -6,21 +6,20 @@ const mongoose = require("mongoose")
 
 
 const chordsSchema = new mongoose.Schema({
-    "song_name": { type: String, required: true, unique: true },
+    "song_name": { type: String, required: true, unique: false },
     "album_name": { type: String, required: false },
-    "cover": { type: Boolean, required: true },
+    "cover": { type: Boolean, required: false },
     "original_singer": { type: String, required: false },
-    "singer": { type: String, required: true },
+    "singer": { type: String, required: false },
     "compostion": { type: String, required: false },
-    "lyricist": { type: String, required: true },
-    "produced_by": { type: String, required: true },
-    "music_director": { type: String, required: true },
+    "lyricist": { type: String, required: false },
+    "produced_by": { type: String, required: false },
+    "music_director": { type: String, required: false },
     "created_time": { type: Date, default: Date.now },
 })
 
 
 
 const creditsSchema = mongoose.model("creditsSchema", chordsSchema)
-
 
 module.exports = creditsSchema
